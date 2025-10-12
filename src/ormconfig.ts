0cd6475abc893a7b488704664f634deabd27170b
blockchainBard101 <bchainbard.annonymousasquare@gmail.com>
2025-10-12 03:00:09 +0100

    db

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,14 +1,22 @@
+import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 const ormConfig: PostgresConnectionOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
-  username: 'devuser',
+  username: 'postgres',
   password: '1234',
-  database: 'blog',
+  database: 'nestjs',
   entities: [__dirname + '/**/*.entity.{ts,js}'],
   synchronize: true, // don't use in production!
+  migrationsTableName: 'migrations',
+  migrations: [__dirname + '/migrations/**/*.ts']
+
 };
+
+const AppDataSource = new DataSource(ormConfig);
+
+export {AppDataSource};
 
 export default ormConfig;
